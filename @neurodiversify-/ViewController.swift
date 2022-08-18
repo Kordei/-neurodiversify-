b0ec3ec22468d7e88e8877b84fa85b5fe093d1f4
Kordei <Kordei.a05@gmail.com>
2022-08-18 04:42:34 -0400

    Initial Commit

---
+++ @neurodiversify-/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  @neurodiversify-
+//
+//  Created by Kordei Aryeetey on 8/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
